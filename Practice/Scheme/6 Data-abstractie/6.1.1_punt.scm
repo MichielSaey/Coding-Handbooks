6f2ed274e543dcad6a16ed07ac14d5f0ceb1ed77
Michiel <michiel.saey1@gmail.com>
2023-10-24 21:00:17 +0200

    opstart oefeninge reelks 6

---
+++ Practice/Scheme/6 Data-abstractie/6.1.1_punt.scm
@@ -0,0 +1,14 @@
+;Vraag
+;Definieer een data-structuur om een (2-dimensionaal) punt voor te stellen. Schrijf hiertoe een constructor
+;(make-punt x y) en de selectoren (x punt) en (y punt).
+
+;> (define a (make-punt 2 11))
+
+;> a
+;(2 . 11)
+
+;> (x a)
+;2
+
+;> (y a)
+;11
